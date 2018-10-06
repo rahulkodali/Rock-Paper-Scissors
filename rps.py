9b3982943dd39a8e2cfb60edcd0e16f0be70063f
rahulkodali <43912726+rahulkodali@users.noreply.github.com>
2018-10-06 11:39:37 -0500

    Update rps.py

--- rps.py
+++ rps.py
@@ -1,14 +1,7 @@
-#userinput = input("prompt>")
-#print("This is how you print stuff")
-
 # Rahul, Make me Rock, Paper, Scissors
 import random
 
 print("This is the rpc game")
 question = input("Do you want rock, paper, or scissors?")
 
-
 computerselection = random.randint(0, 2)
-
-
-# if input = 
