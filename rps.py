d82f371d7e893e83fb9d18e1423d976f8d205cb3
rahulkodali <43912726+rahulkodali@users.noreply.github.com>
2018-10-06 11:50:19 -0500

    Update rps.py

--- rps.py
+++ rps.py
@@ -1,7 +1,16 @@
+#userinput = input("prompt>")
+#print("This is how you print stuff")
+
 # Rahul, Make me Rock, Paper, Scissors
 import random
 
 print("This is the rpc game")
-question = input("Do you want rock, paper, or scissors?")
+playerselection = raw_input("Do you want rock, paper, or scissors?")
 
 computerselection = random.randint(0, 2)
+if computerselection == 0:
+  computerselection = "rock"
+elif computerselection == 1:
+  computerselection = "paper"
+elif computerselection == 2:
+  computerselection = "scissors"
