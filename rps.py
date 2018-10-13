f77a8c527fc6edb58b57b48914b203dd165e9a84
rahulkodali <43912726+rahulkodali@users.noreply.github.com>
2018-10-13 11:50:36 -0500

    Update rps.py

--- rps.py
+++ rps.py
@@ -4,40 +4,38 @@
 # Rahul, Make me Rock, Paper, Scissors
 import random
 
-print("This is the rpc game")
-playerselection = raw_input("Do you want rock, paper, or scissors?")
+print("This is the rps game")
+numofgames = raw_input("How many games do you want to play?")
+numofgames = int(numofgames)
+currentgame = 1
+while currentgame <= numofgames:
 
-computerselection = random.randint(0, 2)
-if computerselection == 0:
-  computerselection = "rock"
-elif computerselection == 1:
-  computerselection = "paper"
-elif computerselection == 2:
-  computerselection = "scissors"
+  playerselection = raw_input("Do you want rock, paper, or scissors?")
 
-if playerselection == "rock" and computerselection == "paper":
-  print("I win, I chose paper")
+  computerselection = random.randint(0, 2)
+  if computerselection == 0:
+    computerselection = "rock"
+  elif computerselection == 1:
+    computerselection = "paper"
+  elif computerselection == 2:
+    computerselection = "scissors"
 
-if playerselection == "paper" and computerselection == "paper":
-  print("we tied, I chose paper")
-
-if playerselection == "scissors" and computerselection == "paper":
-  print("You win, I chose paper")
-
-if playerselection == "rock" and computerselection == "scissors":
-  print("you win, I chose scissors")
-
-if playerselection == "paper" and computerselection == "scissors":
-  print("I win, I chose scissors")
-
-if playerselection == "scissors" and computerselection == "scissors":
-  print("we tied, I chose scissors")
-
-if playerselection == "rock" and computerselection == "rock":
-  print("we tied, I chose rock")
-
-if playerselection == "paper" and computerselection == "rock":
-  print("you win, I chose rock")
-
-if playerselection == "scissors" and computerselection == "rock":
-  print("I win, I chose rock")
+  if playerselection == "rock" and computerselection == "paper":
+    print("I win, I chose paper")
+  elif playerselection == "paper" and computerselection == "paper":
+    print("we tied, I chose paper")
+  elif playerselection == "scissors" and computerselection == "paper":
+    print("You win, I chose paper")
+  elif playerselection == "rock" and computerselection == "scissors":
+    print("you win, I chose scissors")
+  elif playerselection == "paper" and computerselection == "scissors":
+    print("I win, I chose scissors")
+  elif playerselection == "scissors" and computerselection == "scissors":
+    print("we tied, I chose scissors")
+  elif playerselection == "rock" and computerselection == "rock":
+    print("we tied, I chose rock")
+  elif playerselection == "paper" and computerselection == "rock":
+    print("you win, I chose rock")
+  elif playerselection == "scissors" and computerselection == "rock":
+    print("I win, I chose rock")
+  currentgame = currentgame + 1
